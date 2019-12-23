1870fe6b36fd81d2be8cf35a4295fd975ddb2d44
Brom <warfard34@gmail.com>
2019-12-23 23:10:17 +0300

    let's party begin!

--- honeypcapanalyser.py
+++ honeypcapanalyser.py
@@ -1,1 +1,29 @@
+import argparse
 from pcapfile import savefile
+import os
+
+
+def get_pcaps_list(path):
+    if os.path.isdir(path):
+        result = []
+        for file in os.listdir(path):
+            if os.path.isfile(
+                os.path.join(path, file)
+            ) and "pcap" in os.path.splitext(
+                os.path.join(path, file)
+            )[1]:
+                result.append(os.path.join(path, file))
+    elif os.path.isfile(path):
+        result.append(path)
+    return result
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create simple honeypot report from pcap files')
+    parser.add_argument('-i', "--input", help="pcap file or dirrectory with files", required=True)
+    args = parser.parse_args()
+    main(args)
